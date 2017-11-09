89c67f42f0e50d72c0daebd18107d06e7598e794
DESKTOP-GIKQ04K\ckchiruka <ckchiruka@gmail.com>
2017-11-08 17:11:13 -0800

    misplaced file again

--- ML/euclidean/euclideandistance.py
+++ ML/euclidean/euclideandistance.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pylab as plt
 from scipy.spatial.distance import pdist, squareform
+import scipy.stats as st
 from sklearn import manifold
 
 
@@ -13,8 +14,12 @@ def readCleanData():
     JokeRating = pd.read_sql_query("SELECT * from JokeRating", jokedb)
     
     JokeRater = JokeRater.drop(['joke_submitter_id'], axis = 1).set_index('id')
+    #JokeRater['age'] = st.norm.cdf((JokeRater['age'] - JokeRater['age'].mean())/JokeRater['age'].std(ddof=0))
+    #JokeRater['preferred_jokes'] = JokeRater['preferred_joke_genre'].map(str) + JokeRater['preferred_joke_genre2']
+    
+    Joke = Joke.set_index('id')
     JokeRating = JokeRating.drop_duplicates(['joke_id', 'joke_rater_id'], keep = 'first')
-    JokeRating = JokeRating.pivot(index = 'joke_id', columns = 'joke_rater_id', values = 'rating').transpose().fillna(0)
+    JokeRating = JokeRating.pivot(index = 'joke_id', columns = 'joke_rater_id', values = 'rating').transpose().fillna(2.5)
     return JokeRater, JokeRating, Joke
 
 def getEuclideanDistance(JokeRating):
@@ -24,11 +29,10 @@ def getEuclideanDistance(JokeRating):
 
 def distance_map(data):
     '''
-    this function takes in a distance matrix and the clusters and outputs a plot
+    this function takes in a distance matrix and outputs a plot
     inputs: data: a SQUARE distance matrix
-            cluster: the cluster that each index belongs to, in a DataFrame or list
     
-    outputs: a distance plot that color-codes clusters, for up to 9 clusters (more can be added if needed)
+    outputs: a distance plot
     '''
     #http://baoilleach.blogspot.com/2014/01/convert-distance-matrix-to-2d.html
     adist = np.array(data)
@@ -45,7 +49,7 @@ def distance_map(data):
 def userInput():
     #just for now, ideally we need drop down menus
     gender = input('What is your gender? ').strip()
-    age = input('What is your age? ').strip()
+    age = int(input('What is your age? ').strip())
     birth_country = input('What is your birth country? ').strip()
     major = input('What is your major? ').strip()
     joke_genre1 = input('What is your primary preferred joke genre? ').strip()
@@ -53,30 +57,45 @@ def userInput():
     joke_type = input('What is your preferred joke type? ').strip()
     music_genre = input('What is your favorite music genre? ').strip()
     movie_genre = input('What is your favorite movie genre? ').strip()
+    
     return [gender, age, birth_country, major, joke_genre1, joke_genre2, joke_type,
             music_genre, movie_genre]
+  
     
-def raterSimilarity(JokeRater, someGuy):
+def raterSimilarity(JokeRater, someGuy, JokeRating):
     similarity = []
-    for i in JokeRater.index:
+    for i in JokeRating.index:
         k = 0
         sim = 0
         for j in JokeRater.loc[i]:
             if j == someGuy[k]:
                 sim = sim + 1
             k = k + 1
-        similarity.append(sim)
-        
+        similarity.append(sim)    
     return similarity
-            
+
+def preliminaryJokeRatings(similarity, JokeRating):
+    return JokeRating.transpose().dot(similarity)
     
+def printRatings(ratings, Joke):
+    ratings = ratings.sort_values(ascending = False)
+    new_user = []
+    for i in ratings.index:
+        print('\nJoke: ')
+        print(Joke.loc[i].joke_text)
+        new_user.append([input("What is your rating? ").strip(), i])
+        leave = input("Do you want to exit? " ).strip()
+        if leave == "True":
+            break
+    return new_user
+
 def main():
     someGuy = userInput()
     JokeRater, JokeRating, Joke = readCleanData()
     df_dist = getEuclideanDistance(JokeRating)
-    similarity = raterSimilarity(JokeRater, someGuy)
-    print(similarity)
+    similarity = raterSimilarity(JokeRater, someGuy, JokeRating)
+    ratings = preliminaryJokeRatings(similarity, JokeRating)
+    new_user = printRatings(ratings, Joke)
     #distance_map(df_dist)
     
-
 if __name__ == "__main__": main()
