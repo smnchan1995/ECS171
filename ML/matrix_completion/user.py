c35e00711de993b13f7a5b01cd30d380238e1088
Dominic Yang <domyang@ucdavis.edu>
2017-11-13 17:26:36 -0800

    Reading user input now works properly

--- ML/matrix_completion/user.py
+++ ML/matrix_completion/user.py
@@ -4,8 +4,52 @@ user.py
 This should include basic information about users
 """
 
+import sqlite3 as sql
+
+import pandas as pd
+
 from forms import IntegerField, CategoricalField
-from matrix_complete import read_clean_data
+
+def read_clean_data():
+    """
+    This function will pull from the sqlite database 'jokedb.sqlite3' data
+    related to the joke raters, joke ratings, and jokes themselves.
+    It will minimally format the data, removing duplicate entries.
+
+    It will return three DataFrames the first containing information
+    regarding the joke raters, the second containing information about the
+    joke ratings, and the third containing information about the jokes.
+
+    joke_rater has columns:
+    gender, age, birth_country, major, preferred_joke_genre,
+    preferred_joke_genre2, preferred_joke_type, favorite_music_genre,
+    favorite_movie_genre
+
+    joke_ratings is a dataframe with columns indexing each joke and the row
+    index being the index of the user rating the joke.
+
+    jokes has columns:
+    category, joke_type, subject, joke_text, joke_submitter_id, joke_source
+
+    Returns:
+        tuple(DataFrame, DataFrame, DataFrame): Three dataframes in order,
+            joke raters, joke ratings, jokes
+    """
+    jokedb = sql.connect("jokedb.sqlite3")
+    joke_rater = pd.read_sql_query("SELECT * from JokeRater", jokedb)
+    joke = pd.read_sql_query("SELECT * from Joke", jokedb)
+    joke_rating = pd.read_sql_query("SELECT * from JokeRating", jokedb)
+
+    joke_rater = joke_rater.drop(['joke_submitter_id'], axis=1).set_index('id')
+
+    joke = joke.set_index('id')
+    joke_rating = joke_rating.drop_duplicates(['joke_id', 'joke_rater_id'],
+                                            keep='first')
+    pivot = joke_rating.pivot(index='joke_id', columns='joke_rater_id',
+                              values='rating')
+    joke_rating = pivot.transpose()
+
+    return joke_rater, joke_rating, joke
 
 joke_raters, _, _ = read_clean_data()
 
